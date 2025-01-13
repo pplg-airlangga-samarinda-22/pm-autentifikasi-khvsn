68a18c4c6615326c2c980f96daee3a4e4c26beec
khvsn <nasir.25477@gmail.com>
2025-01-13 13:27:09 +0800

    done

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "pengaduan_masyarakat";
+
+$koneksi = new mysqli($hostname, $username, $password, $database);
+?>
